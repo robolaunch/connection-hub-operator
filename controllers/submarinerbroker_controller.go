e3b72e9a62b164c50d283e46a93b66f555acf937
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-10-20 19:44:21 +0300

    refactor(controller): :construction: add default controller functions to submarinerbroker controller

--- controllers/submarinerbroker_controller.go
+++ controllers/submarinerbroker_controller.go
@@ -3,11 +3,15 @@ package controllers
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	connectionhubv1alpha1 "github.com/robolaunch/connection-hub-operator/api/v1alpha1"
 )
 
@@ -21,10 +25,77 @@ type SubmarinerBrokerReconciler struct {
 //+kubebuilder:rbac:groups=connection-hub.roboscale.io,resources=submarinerbrokers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=connection-hub.roboscale.io,resources=submarinerbrokers/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch;create;update;patch;delete
+
+var logger logr.Logger
+
 func (r *SubmarinerBrokerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger = log.FromContext(ctx)
+
+	instance, err := r.smbReconcileGetInstance(ctx, req.NamespacedName)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	err = r.smbReconcileCheckStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.smbReconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.smbReconcileCheckResources(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.smbReconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *SubmarinerBrokerReconciler) smbReconcileCheckStatus(ctx context.Context, instance *connectionhubv1alpha1.SubmarinerBroker) error {
+	return nil
+}
+
+func (r *SubmarinerBrokerReconciler) smbReconcileCheckResources(ctx context.Context, instance *connectionhubv1alpha1.SubmarinerBroker) error {
+	return nil
+}
+
+func (r *SubmarinerBrokerReconciler) smbReconcileGetInstance(ctx context.Context, meta types.NamespacedName) (*connectionhubv1alpha1.SubmarinerBroker, error) {
+	instance := &connectionhubv1alpha1.SubmarinerBroker{}
+	err := r.Get(ctx, meta, instance)
+	if err != nil {
+		return &connectionhubv1alpha1.SubmarinerBroker{}, err
+	}
+
+	return instance, nil
+}
+
+func (r *SubmarinerBrokerReconciler) smbReconcileUpdateInstanceStatus(ctx context.Context, instance *connectionhubv1alpha1.SubmarinerBroker) error {
+	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		instanceLV := &connectionhubv1alpha1.SubmarinerBroker{}
+		err := r.Get(ctx, types.NamespacedName{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+		}, instanceLV)
+
+		if err == nil {
+			instance.ResourceVersion = instanceLV.ResourceVersion
+		}
+
+		err1 := r.Status().Update(ctx, instance)
+		return err1
+	})
 }
 
 // SetupWithManager sets up the controller with the Manager.
