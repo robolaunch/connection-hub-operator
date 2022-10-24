8b7bac04f19b5c05622dad5cc39345950370087f
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-10-24 16:14:55 +0300

    refactor(api): :mag: create spec and status fields of submarineroperator

--- api/v1alpha1/submarineroperator_types.go
+++ api/v1alpha1/submarineroperator_types.go
@@ -4,12 +4,37 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+const (
+	SubmarinerOperatorNamespace string = "submariner-operator"
+)
+
 // SubmarinerOperatorSpec defines the desired state of SubmarinerOperator
 type SubmarinerOperatorSpec struct {
+	// +kubebuilder:validation:Required
+	ClusterCIDR string `json:"clusterCIDR"`
+	// +kubebuilder:validation:Required
+	ServiceCIDR string `json:"serviceCIDR"`
+	// +kubebuilder:validation:Required
+	PresharedKey string `json:"presharedKey"`
+	// +kubebuilder:validation:Required
+	Broker BrokerInfo `json:"broker"`
+	// +kubebuilder:validation:Required
+	ClusterID string `json:"clusterID"`
 }
+
+type SubmarinerOperatorPhase string
+
+const (
+	SubmarinerOperatorPhaseNotExists     SubmarinerOperatorPhase = "NotExists"
+	SubmarinerOperatorPhaseDeploying     SubmarinerOperatorPhase = "Deploying"
+	SubmarinerOperatorPhaseRunning       SubmarinerOperatorPhase = "Running"
+	SubmarinerOperatorPhaseMalfunctioned SubmarinerOperatorPhase = "Malfunctioned"
+)
 
 // SubmarinerOperatorStatus defines the observed state of SubmarinerOperator
 type SubmarinerOperatorStatus struct {
+	// +kubebuilder:default="NotExists"
+	Phase SubmarinerOperatorPhase `json:"phase,omitempty"`
 }
 
 //+kubebuilder:object:root=true
--- api/v1alpha1/submarineroperator_webhook.go
+++ api/v1alpha1/submarineroperator_webhook.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2022.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
@@ -32,8 +16,6 @@ func (r *SubmarinerOperator) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
 //+kubebuilder:webhook:path=/mutate-connection-hub-roboscale-io-v1alpha1-submarineroperator,mutating=true,failurePolicy=fail,sideEffects=None,groups=connection-hub.roboscale.io,resources=submarineroperators,verbs=create;update,versions=v1alpha1,name=msubmarineroperator.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Defaulter = &SubmarinerOperator{}
@@ -41,11 +23,8 @@ var _ webhook.Defaulter = &SubmarinerOperator{}
 // Default implements webhook.Defaulter so a webhook will be registered for the type
 func (r *SubmarinerOperator) Default() {
 	submarineroperatorlog.Info("default", "name", r.Name)
-
-	// TODO(user): fill in your defaulting logic.
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 //+kubebuilder:webhook:path=/validate-connection-hub-roboscale-io-v1alpha1-submarineroperator,mutating=false,failurePolicy=fail,sideEffects=None,groups=connection-hub.roboscale.io,resources=submarineroperators,verbs=create;update,versions=v1alpha1,name=vsubmarineroperator.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &SubmarinerOperator{}
@@ -53,23 +32,17 @@ var _ webhook.Validator = &SubmarinerOperator{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *SubmarinerOperator) ValidateCreate() error {
 	submarineroperatorlog.Info("validate create", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object creation.
 	return nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *SubmarinerOperator) ValidateUpdate(old runtime.Object) error {
 	submarineroperatorlog.Info("validate update", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object update.
 	return nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *SubmarinerOperator) ValidateDelete() error {
 	submarineroperatorlog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }
