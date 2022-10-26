937b3d8338e296bb622bc02d7465bd156fbbac3c
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-10-26 16:46:49 +0300

    refactor(submariner): :high_brightness: add default controller functions

--- controllers/submariner_controller.go
+++ controllers/submariner_controller.go
@@ -2,8 +2,15 @@ package controllers
 
 import (
 	"context"
+	basicErr "errors"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -22,9 +29,122 @@ type SubmarinerReconciler struct {
 // +kubebuilder:rbac:groups=connection-hub.roboscale.io,resources=submariners/finalizers,verbs=update
 
 func (r *SubmarinerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger = log.FromContext(ctx)
+
+	instance, err := r.submarinerReconcileGetInstance(ctx, req.NamespacedName)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	err = r.submarinerReconcileCheckNode(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.submarinerReconcileCheckStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.submarinerReconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.submarinerReconcileCheckResources(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.submarinerReconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *SubmarinerReconciler) submarinerReconcileCheckStatus(ctx context.Context, instance *connectionhubv1alpha1.Submariner) error {
+	return nil
+}
+
+func (r *SubmarinerReconciler) submarinerReconcileCheckResources(ctx context.Context, instance *connectionhubv1alpha1.Submariner) error {
+	return nil
+}
+
+func (r *SubmarinerReconciler) submarinerReconcileCheckNode(ctx context.Context, instance *connectionhubv1alpha1.Submariner) error {
+	tenancy := connectionhubv1alpha1.GetTenancySelectorsForSubmariner(*instance)
+
+	instance.Status.NodeInfo.Selectors = make(map[string]string)
+
+	if tenancy.RobolaunchCloudInstance != "" {
+		instance.Status.NodeInfo.Selectors[connectionhubv1alpha1.RobolaunchCloudInstanceLabelKey] = tenancy.RobolaunchCloudInstance
+	}
+
+	if tenancy.RobolaunchPhysicalInstance != "" {
+		instance.Status.NodeInfo.Selectors[connectionhubv1alpha1.RobolaunchPhysicalInstanceLabelKey] = tenancy.RobolaunchCloudInstance
+	}
+
+	requirements := []labels.Requirement{}
+	requirementsMap := instance.Status.NodeInfo.Selectors
+	for k, v := range requirementsMap {
+		newReq, err := labels.NewRequirement(k, selection.In, []string{v})
+		if err != nil {
+			return err
+		}
+		requirements = append(requirements, *newReq)
+	}
+
+	nodeSelector := labels.NewSelector().Add(requirements...)
+
+	nodes := &corev1.NodeList{}
+	err := r.List(ctx, nodes, &client.ListOptions{
+		LabelSelector: nodeSelector,
+	})
+	if err != nil {
+		return err
+	}
+
+	if len(nodes.Items) == 0 {
+		return basicErr.New("no nodes are listed with node selector")
+	} else if len(nodes.Items) > 1 {
+		return basicErr.New("multiple nodes are listed, no specific target")
+	}
+
+	node := nodes.Items[0]
+	instance.Status.NodeInfo.Name = node.Name
+
+	return nil
+}
+
+func (r *SubmarinerReconciler) submarinerReconcileGetInstance(ctx context.Context, meta types.NamespacedName) (*connectionhubv1alpha1.Submariner, error) {
+	instance := &connectionhubv1alpha1.Submariner{}
+	err := r.Get(ctx, meta, instance)
+	if err != nil {
+		return &connectionhubv1alpha1.Submariner{}, err
+	}
+
+	return instance, nil
+}
+
+func (r *SubmarinerReconciler) submarinerReconcileUpdateInstanceStatus(ctx context.Context, instance *connectionhubv1alpha1.Submariner) error {
+	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		instanceLV := &connectionhubv1alpha1.Submariner{}
+		err := r.Get(ctx, types.NamespacedName{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+		}, instanceLV)
+
+		if err == nil {
+			instance.ResourceVersion = instanceLV.ResourceVersion
+		}
+
+		err1 := r.Status().Update(ctx, instance)
+		return err1
+	})
 }
 
 // SetupWithManager sets up the controller with the Manager.
