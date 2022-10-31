e6beb6666be8da4201394e4a8d68edc1ee227c1e
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-10-31 10:18:15 +0300

    refactor(cloud-instance): :eyes: watch submariner cr and set status

--- controllers/cloudinstance_controller.go
+++ controllers/cloudinstance_controller.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"context"
-	"time"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -10,6 +9,9 @@ import (
 	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	connectionhubv1alpha1 "github.com/robolaunch/connection-hub-operator/api/v1alpha1"
 )
@@ -24,7 +26,12 @@ type CloudInstanceReconciler struct {
 //+kubebuilder:rbac:groups=connection-hub.roboscale.io,resources=cloudinstances/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=connection-hub.roboscale.io,resources=cloudinstances/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=connection-hub.roboscale.io,resources=submariners,verbs=get;list;watch;create;update;patch;delete
+
 func (r *CloudInstanceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+
+	logger.Info("Reconciling Cloud Instance")
+
 	instance, err := r.reconcileGetInstance(ctx, req.NamespacedName)
 	if err != nil {
 		if errors.IsNotFound(err) {
@@ -53,17 +60,49 @@ func (r *CloudInstanceReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 		return ctrl.Result{}, err
 	}
 
-	return ctrl.Result{
-		Requeue:      true,
-		RequeueAfter: 5 * time.Second,
-	}, nil
+	return ctrl.Result{}, nil
 }
 
 func (r *CloudInstanceReconciler) reconcileCheckStatus(ctx context.Context, instance *connectionhubv1alpha1.CloudInstance) error {
+
+	switch instance.Status.DeployerStatus.Exists {
+	case true:
+
+		switch instance.Status.DeployerStatus.Phase {
+		case connectionhubv1alpha1.SubmarinerPhaseReadyToConnect:
+
+			instance.Status.Phase = connectionhubv1alpha1.CloudInstancePhaseTryingToConnect
+
+		default:
+
+			instance.Status.Phase = connectionhubv1alpha1.CloudInstancePhaseWaitingForDeployer
+
+		}
+
+	case false:
+		instance.Status.Phase = connectionhubv1alpha1.CloudInstancePhaseLookingForDeployer
+	}
+
 	return nil
 }
 
 func (r *CloudInstanceReconciler) reconcileCheckResources(ctx context.Context, instance *connectionhubv1alpha1.CloudInstance) error {
+
+	instance.Status.DeployerStatus.Name = instance.GetSubmarinerDeployerMetadata().Name
+
+	submarinerDeployer := &connectionhubv1alpha1.Submariner{}
+	err := r.Get(ctx, instance.GetSubmarinerDeployerMetadata(), submarinerDeployer)
+	if err != nil && errors.IsNotFound(err) {
+		instance.Status.DeployerStatus = connectionhubv1alpha1.DeployerStatus{}
+	} else if err != nil {
+		return err
+	} else {
+
+		instance.Status.DeployerStatus.Exists = true
+		instance.Status.DeployerStatus.Phase = submarinerDeployer.Status.Phase
+
+	}
+
 	return nil
 }
 
@@ -96,7 +135,31 @@ func (r *CloudInstanceReconciler) reconcileUpdateInstanceStatus(ctx context.Cont
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *CloudInstanceReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&connectionhubv1alpha1.CloudInstance{}).
+		Watches(
+			&source.Kind{Type: &connectionhubv1alpha1.Submariner{}},
+			handler.EnqueueRequestsFromMapFunc(r.watchSubmarinerDeployer),
+		).
 		Complete(r)
+}
+
+func (r *CloudInstanceReconciler) watchSubmarinerDeployer(o client.Object) []reconcile.Request {
+	cloudInstances := &connectionhubv1alpha1.CloudInstanceList{}
+	err := r.List(context.TODO(), cloudInstances)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+
+	requests := make([]reconcile.Request, len(cloudInstances.Items))
+	for i, item := range cloudInstances.Items {
+		requests[i] = reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name: item.Name,
+			},
+		}
+	}
+
+	return requests
 }
