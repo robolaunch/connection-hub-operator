031f690ec7f04f0723e3c842feb78488476f6b43
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-11-02 12:22:43 +0300

    refactor(federation-operator): add default controller functions for federation operator

--- controllers/federationoperator_controller.go
+++ controllers/federationoperator_controller.go
@@ -3,7 +3,10 @@ package controllers
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -21,10 +24,79 @@ type FederationOperatorReconciler struct {
 //+kubebuilder:rbac:groups=connection-hub.roboscale.io,resources=federationoperators/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=connection-hub.roboscale.io,resources=federationoperators/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=multiclusterdns.kubefed.io,resources=*,verbs=*
+//+kubebuilder:rbac:groups=scheduling.kubefed.io,resources=*,verbs=*
+//+kubebuilder:rbac:groups=core.kubefed.io,resources=*,verbs=*
+//+kubebuilder:rbac:groups=types.kubefed.io,resources=*,verbs=*
+
 func (r *FederationOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger = log.FromContext(ctx)
+
+	instance, err := r.reconcileGetInstance(ctx, req.NamespacedName)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileCheckStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileCheckResources(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *FederationOperatorReconciler) reconcileCheckStatus(ctx context.Context, instance *connectionhubv1alpha1.FederationOperator) error {
+
+	return nil
+}
+
+func (r *FederationOperatorReconciler) reconcileCheckResources(ctx context.Context, instance *connectionhubv1alpha1.FederationOperator) error {
+	return nil
+}
+
+func (r *FederationOperatorReconciler) reconcileGetInstance(ctx context.Context, meta types.NamespacedName) (*connectionhubv1alpha1.FederationOperator, error) {
+	instance := &connectionhubv1alpha1.FederationOperator{}
+	err := r.Get(ctx, meta, instance)
+	if err != nil {
+		return &connectionhubv1alpha1.FederationOperator{}, err
+	}
+
+	return instance, nil
+}
+
+func (r *FederationOperatorReconciler) reconcileUpdateInstanceStatus(ctx context.Context, instance *connectionhubv1alpha1.FederationOperator) error {
+	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		instanceLV := &connectionhubv1alpha1.FederationOperator{}
+		err := r.Get(ctx, types.NamespacedName{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+		}, instanceLV)
+
+		if err == nil {
+			instance.ResourceVersion = instanceLV.ResourceVersion
+		}
+
+		err1 := r.Status().Update(ctx, instance)
+		return err1
+	})
 }
 
 // SetupWithManager sets up the controller with the Manager.
