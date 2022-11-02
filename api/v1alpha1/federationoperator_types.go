1b7432b11c2fd64508dbb1808bad531eb35f9a16
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-11-02 11:56:22 +0300

    refactor(api): :chart_with_upwards_trend: add chart spec and resource status to api

--- api/v1alpha1/federationoperator_types.go
+++ api/v1alpha1/federationoperator_types.go
@@ -4,12 +4,35 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+const (
+	FederationOperatorNamespace string = "kube-federation-system"
+)
+
 // FederationOperatorSpec defines the desired state of FederationOperator
 type FederationOperatorSpec struct {
+	// +kubebuilder:validation:Required
+	Helm HelmChartProperties `json:"helm"`
 }
+
+type FederationOperatorPhase string
+
+const (
+	FederationOperatorPhaseCreatingNamespace FederationOperatorPhase = "CreatingNamespace"
+	FederationOperatorPhaseDeployingChart    FederationOperatorPhase = "DeployingChart"
+	FederationOperatorPhaseCheckingResources FederationOperatorPhase = "CheckingResources"
+	FederationOperatorPhaseDeployed          FederationOperatorPhase = "Deployed"
+	FederationOperatorPhaseMalfunctioned     FederationOperatorPhase = "Malfunctioned"
+
+	FederationOperatorPhaseUninstallingChart    FederationOperatorPhase = "UninstallingChart"
+	FederationOperatorPhaseTerminatingNamespace FederationOperatorPhase = "TerminatingNamespace"
+)
 
 // FederationOperatorStatus defines the observed state of FederationOperator
 type FederationOperatorStatus struct {
+	NamespaceStatus     NamespaceStatus         `json:"namespaceStatus,omitempty"`
+	ChartStatus         ChartStatus             `json:"chartStatus,omitempty"`
+	ChartResourceStatus ChartResourceStatus     `json:"chartResourceStatus,omitempty"`
+	Phase               FederationOperatorPhase `json:"phase,omitempty"`
 }
 
 //+kubebuilder:object:root=true
