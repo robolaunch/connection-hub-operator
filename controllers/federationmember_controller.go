1fa1d6acf61b4e1a7ebc23c940ca7b52b9ff13ee
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-11-03 14:59:50 +0300

    refactor(federation-member): add default reconciler functions

--- controllers/federationmember_controller.go
+++ controllers/federationmember_controller.go
@@ -3,7 +3,10 @@ package controllers
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -23,9 +26,72 @@ type FederationMemberReconciler struct {
 
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *FederationMemberReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger = log.FromContext(ctx)
+
+	instance, err := r.reconcileGetInstance(ctx, req.NamespacedName)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileCheckStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileCheckResources(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *FederationMemberReconciler) reconcileCheckStatus(ctx context.Context, instance *connectionhubv1alpha1.FederationMember) error {
+	return nil
+}
+
+func (r *FederationMemberReconciler) reconcileCheckResources(ctx context.Context, instance *connectionhubv1alpha1.FederationMember) error {
+	return nil
+}
+
+func (r *FederationMemberReconciler) reconcileGetInstance(ctx context.Context, meta types.NamespacedName) (*connectionhubv1alpha1.FederationMember, error) {
+	instance := &connectionhubv1alpha1.FederationMember{}
+	err := r.Get(ctx, meta, instance)
+	if err != nil {
+		return &connectionhubv1alpha1.FederationMember{}, err
+	}
+
+	return instance, nil
+}
+
+func (r *FederationMemberReconciler) reconcileUpdateInstanceStatus(ctx context.Context, instance *connectionhubv1alpha1.FederationMember) error {
+	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		instanceLV := &connectionhubv1alpha1.FederationMember{}
+		err := r.Get(ctx, types.NamespacedName{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+		}, instanceLV)
+
+		if err == nil {
+			instance.ResourceVersion = instanceLV.ResourceVersion
+		}
+
+		err1 := r.Status().Update(ctx, instance)
+		return err1
+	})
 }
 
 // SetupWithManager sets up the controller with the Manager.
