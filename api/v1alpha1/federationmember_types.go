5a7c3c8b516317004a25d3be97ad9387a57e4d9e
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-11-05 18:10:23 +0300

    refactor(federation-member): add phases for member

--- api/v1alpha1/federationmember_types.go
+++ api/v1alpha1/federationmember_types.go
@@ -3,6 +3,8 @@ package v1alpha1
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/kubefed/pkg/apis/core/common"
 )
 
 type FederationMemberCredentials struct {
@@ -22,9 +24,29 @@ type FederationMemberSpec struct {
 	Credentials FederationMemberCredentials `json:"credentials"`
 }
 
+type FederationMemberPhase string
+
+const (
+	FederationMemberPhaseJoiningFederation FederationMemberPhase = "JoiningFederation"
+	FederationMemberPhaseOffline           FederationMemberPhase = "Offline"
+	FederationMemberPhaseReady             FederationMemberPhase = "Ready"
+	FederationMemberPhaseMalfunctioned     FederationMemberPhase = "Malfunctioned"
+
+	FederationMemberPhaseCannotJoinFederation FederationMemberPhase = "CannotJoinFederation"
+	FederationMemberPhaseUnjoiningFederation  FederationMemberPhase = "UnjoiningFederation"
+)
+
+type KubeFedClusterStatus struct {
+	Created       bool                        `json:"created,omitempty"`
+	ConditionType common.ClusterConditionType `json:"conditionType,omitempty"`
+	Reason        string                      `json:"reason,omitempty"`
+}
+
 // FederationMemberStatus defines the observed state of FederationMember
 type FederationMemberStatus struct {
-	Joined bool `json:"joined,omitempty"`
+	JoinAttempted        bool                  `json:"joinAttempted,omitempty"`
+	KubeFedClusterStatus KubeFedClusterStatus  `json:"kubefedClusterStatus,omitempty"`
+	Phase                FederationMemberPhase `json:"phase,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -56,5 +78,95 @@ func init() {
 func (federationmember *FederationMember) GetOwnerMetadata() *types.NamespacedName {
 	return &types.NamespacedName{
 		Name: federationmember.OwnerReferences[0].Name,
+	}
+}
+
+type TargetCluster string
+
+const (
+	TargetClusterHost   TargetCluster = "Host"
+	TargetClusterMember TargetCluster = "Member"
+)
+
+type MulticlusterResourceItem struct {
+	TargetCluster
+	client.ObjectKey
+	GroupVersionKind metav1.GroupVersionKind
+}
+
+func (fm *FederationMember) GetResourcesForCheck() []MulticlusterResourceItem {
+	return []MulticlusterResourceItem{
+		{
+			TargetCluster: TargetClusterMember,
+			ObjectKey: types.NamespacedName{
+				Namespace: FederationOperatorNamespace,
+				Name:      fm.Name + "-" + fm.GetOwnerMetadata().Name,
+			},
+			GroupVersionKind: metav1.GroupVersionKind{
+				Group:   "",
+				Version: "v1",
+				Kind:    "ServiceAccount",
+			},
+		},
+		{
+			TargetCluster: TargetClusterMember,
+			ObjectKey: types.NamespacedName{
+				Namespace: FederationOperatorNamespace,
+				Name:      "kubefed-controller-manager:" + fm.Name + "-" + fm.GetOwnerMetadata().Name,
+			},
+			GroupVersionKind: metav1.GroupVersionKind{
+				Group:   "rbac.authorization.k8s.io",
+				Version: "v1",
+				Kind:    "ClusterRole",
+			},
+		},
+		{
+			TargetCluster: TargetClusterMember,
+			ObjectKey: types.NamespacedName{
+				Namespace: FederationOperatorNamespace,
+				Name:      "kubefed-controller-manager:" + fm.Name + "-" + fm.GetOwnerMetadata().Name,
+			},
+			GroupVersionKind: metav1.GroupVersionKind{
+				Group:   "rbac.authorization.k8s.io",
+				Version: "v1",
+				Kind:    "ClusterRoleBinding",
+			},
+		},
+		// {
+		// 	TargetCluster: TargetClusterMember,
+		// 	ObjectKey: types.NamespacedName{
+		// 		Namespace: FederationOperatorNamespace,
+		// 		Name:      fm.Name + "-" + fm.GetOwnerMetadata().Name + "-token-xv87e",
+		// 	},
+		// 	GroupVersionKind: metav1.GroupVersionKind{
+		// 		Group:   "",
+		// 		Version: "v1",
+		// 		Kind:    "Secret",
+		// 	},
+		// },
+		// {
+		// 	TargetCluster: TargetClusterHost,
+		// 	ObjectKey: types.NamespacedName{
+		// 		Namespace: FederationOperatorNamespace,
+		// 		Name:      fm.Name + "-token-xv87e",
+		// 	},
+		// 	GroupVersionKind: metav1.GroupVersionKind{
+		// 		Group:   "",
+		// 		Version: "v1",
+		// 		Kind:    "Secret",
+		// 	},
+		// },
+		{
+			TargetCluster: TargetClusterHost,
+			ObjectKey: types.NamespacedName{
+				Namespace: FederationOperatorNamespace,
+				Name:      fm.Name,
+			},
+			GroupVersionKind: metav1.GroupVersionKind{
+				Group:   "core.kubefed.io",
+				Version: "v1beta1",
+				Kind:    "KubeFedCluster",
+			},
+		},
 	}
 }
