227eb30ff78e35ce2d10bde72989d6eb571a2cd9
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-11-07 03:06:58 +0300

    build(manifests): :whale: update manifests

--- main.go
+++ main.go
@@ -197,6 +197,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ConnectionHub")
 		os.Exit(1)
 	}
+	if err = (&connectionhubv1alpha1.ConnectionHub{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "ConnectionHub")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
