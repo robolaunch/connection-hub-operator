7aa5fd23313b5ce222a1c3f0fd66b1e4010d19d0
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-01-13 13:06:25 +0300

    refactor(federation): update kubefed values

--- controllers/pkg/helm/federation_operator_values.go
+++ controllers/pkg/helm/federation_operator_values.go
@@ -2,8 +2,15 @@ package helm
 
 import connectionhubv1alpha1 "github.com/robolaunch/connection-hub-operator/api/v1alpha1"
 
+// controllermanager.featureGates.RawResourceStatusCollection
+
+type FeatureGates struct {
+	RawResourceStatusCollection string `yaml:"RawResourceStatusCollection"`
+}
+
 type ControllerManager struct {
 	CommonNodeSelector map[string]string `yaml:"commonNodeSelector"`
+	FeatureGates       FeatureGates      `yaml:"featureGates"`
 }
 
 type FederationOperatorValues struct {
@@ -21,6 +28,7 @@ func getFederationOperatorValuesDefault() FederationOperatorValues {
 func GetFederationOperatorValues(federationOperator connectionhubv1alpha1.FederationOperator) FederationOperatorValues {
 	valuesObj := getFederationOperatorValuesDefault()
 	valuesObj.ControllerManager.CommonNodeSelector = federationOperator.Labels
+	valuesObj.ControllerManager.FeatureGates.RawResourceStatusCollection = "Enabled"
 
 	return valuesObj
 }
